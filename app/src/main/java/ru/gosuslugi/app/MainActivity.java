3b19bf7638f6a94c07f545e27a584b2d001ac6d2
treeeeeer563 <doods58463@gmail.com>
2026-05-13 13:19:21 +0300

    Create MainActivity.java

---
+++ app/src/main/java/ru/gosuslugi/app/MainActivity.java
@@ -0,0 +1,19 @@
+package ru.gosuslugi.app;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView placeholderText = findViewById(R.id.placeholderText);
+        if (placeholderText != null) {
+            placeholderText.setText("Госуслуги загружаются...");
+        }
+    }
+}
